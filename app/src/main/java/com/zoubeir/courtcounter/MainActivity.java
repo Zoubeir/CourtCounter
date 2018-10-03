95c3146097bd70aaa261cd697b5257a3ac224ece
zoubeir <zoubx7@gmail.com>
2018-10-04 02:31:29 +0400

    feat: score buttons for team A

--- app/src/main/java/com/zoubeir/courtcounter/MainActivity.java
+++ app/src/main/java/com/zoubeir/courtcounter/MainActivity.java
@@ -7,7 +7,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int scoreTeamA, getScoreTeamB = 0; //global variables to hold the score for each team
+    int scoreTeamA, scoreTeamB = 0; //global variables to hold the score for each team
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,5 +61,57 @@ public class MainActivity extends AppCompatActivity {
         scoreTeamA += FREE_THROW; //add 1 point to the score
 
         displayForTeamA(scoreTeamA); //display the new score
+    }
+
+    /* ----- Team B ----- */
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * add 3 points to the score when +3 Points button is clicked
+     * @param view +3 Points button
+     */
+    public void addThreePointsTeamB(View view) {
+        final int THREE_POINTS = 3; //constant to hold the score
+
+        scoreTeamB += THREE_POINTS; //add 3 points to the score
+
+        displayForTeamB(scoreTeamB); //display the new score
+    }
+
+    /**
+     * add 2 points to the score when +2 Points button is clicked
+     * @param view +2 Points button
+     */
+    public void addTwoPointsTeamB(View view) {
+        final int TWO_POINTS = 2; //constant to hold the score
+
+        scoreTeamB += TWO_POINTS; //add 2 points to the score
+
+        displayForTeamB(scoreTeamB); //display the new score
+    }
+
+    /**
+     * add 1 point to the score when Free Throw button is clicked
+     * @param view Free Throw button
+     */
+    public void addOnePointTeamB(View view) {
+        final int FREE_THROW = 1; //constant to hold the score
+
+        scoreTeamB += FREE_THROW; //add 1 point to the score
+
+        displayForTeamB(scoreTeamB); //display the new score
+    }
+
+    public void clearScores(View view) {
+        scoreTeamA = scoreTeamB = 0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
     }
 }
